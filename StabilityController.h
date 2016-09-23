fccc301ae90f8503a650aa091f6a41f86def6851
Jackson Kent Rollins <rollins.jackson@gmail.com>
2016-09-23 10:54:08 -0600

    Refactored
    I went through quite a few iterations to get the stabilization down. I
    finally have all the motors working again. There is still debilitating
    wobble but it’s so close. Of course my IMU Breakout died this morning
    so I’m uploading this now until I get a new one next week. Then it’s
    time to find working PID numbers.

--- StabilityController.h
+++ StabilityController.h
@@ -1,98 +1,106 @@
+/**
+ * may run into issues when drone is left sitting for some time because PID is still calculating. Worst case, have PID only run when armed == true -jkr
+ */
+
 #include <PID_v1.h>
 
-#define PID_OUTPUT_MAX 255;
+#define PID_OUTPUT_MAX 255
 
 class StabilityController {
   protected:
     RemoteControl* _rc;
     ROSController* _ros;
     IMUController* _imu;
-    PID* _PIDPitchPos;
-    PID* _PIDRollPos;
-    PID* _PIDPitchNeg;
-    PID* _PIDRollNeg;
-    double _r_yaw;
-    double _pid_pitchp_input, _pid_pitchp_output, _pid_pitchp_setpoint;
-    double _pid_pitchn_input, _pid_pitchn_output, _pid_pitchn_setpoint;
-    double _pid_rollp_input, _pid_rollp_output, _pid_rollp_setpoint;
-    double _pid_rolln_input, _pid_rolln_output, _pid_rolln_setpoint;
+    PID* _PIDPitch;
+    PID* _PIDRoll;
+    PID* _PIDPitchOpp;
+    PID* _PIDRollOpp;
+    float _r_yaw;
+    double _pid_pitch_input, _pid_pitch_output, _pid_pitch_setpoint;
+    double _pid_roll_input, _pid_roll_output, _pid_roll_setpoint;
+    double _pid_pitchopp_input, _pid_pitchopp_output, _pid_pitchopp_setpoint;
+    double _pid_rollopp_input, _pid_rollopp_output, _pid_rollopp_setpoint;
   public:
     StabilityController(RemoteControl* rc, ROSController* ros, IMUController *imu);
     void loop();
-    const double PITCH_MAX = 25;
-    const double ROLL_MAX = 25;
-    const double YAW_MAX = 5;
-    double* getPRYOutput();
+    const int PITCH_MAX = 25;
+    const int ROLL_MAX = 25;
+    const int YAW_MAX = 5;
+    float* getOutput();
 
-    static const double PID_KP;
-    static const double PID_KI;
-    static const double PID_KD;
-    static const double GYRO_MULT_PITCH;
-    static const double GYRO_MULT_ROLL;
-    static const double GYRO_MULT_YAW;
+    static const float PID_KP;
+    static const float PID_KI;
+    static const float PID_KD;
+    static const float PID_MULT;
+    static const float GYRO_MULT_PITCH;
+    static const float GYRO_MULT_ROLL;
+    static const float GYRO_MULT_YAW;
 };
 
-    const double StabilityController::PID_KP =            4.00; // proportional
-    const double StabilityController::PID_KI =            1.00; // integral
-    const double StabilityController::PID_KD =            0.00; // derivative
-    const double StabilityController::GYRO_MULT_PITCH =   0.50; // influence of gyro on output
-    const double StabilityController::GYRO_MULT_ROLL =    0.50;
-    const double StabilityController::GYRO_MULT_YAW =     0.50;
+    const float StabilityController::PID_KP =            0.0; // proportional
+    const float StabilityController::PID_KI =            0.0; // integral
+    const float StabilityController::PID_KD =            0.00; // derivative
+    const float StabilityController::PID_MULT =          0.50; // influence of pid on output
+    const float StabilityController::GYRO_MULT_PITCH =   0.00; // influence of gyro on output
+    const float StabilityController::GYRO_MULT_ROLL =    0.00;
+    const float StabilityController::GYRO_MULT_YAW =     0.00;
 
 StabilityController::StabilityController(RemoteControl* rc, ROSController* ros, IMUController *imu) {
   this->_rc = rc;
   this->_ros = ros;
   this->_imu = imu;
   
-  this->_PIDPitchPos = new PID(&this->_pid_pitchp_input, 
-                            &this->_pid_pitchp_output, 
-                            &this->_pid_pitchp_setpoint, 
+  this->_PIDPitch = new PID(&this->_pid_pitch_input, 
+                            &this->_pid_pitch_output, 
+                            &this->_pid_pitch_setpoint, 
                             StabilityController::PID_KP, 
                             StabilityController::PID_KI, 
                             StabilityController::PID_KD, 
                             DIRECT
                           );
-  this->_PIDRollPos = new PID(&this->_pid_rollp_input, 
-                             &this->_pid_rollp_output, 
-                             &this->_pid_rollp_setpoint, 
+  this->_PIDRoll = new PID(&this->_pid_roll_input, 
+                             &this->_pid_roll_output, 
+                             &this->_pid_roll_setpoint, 
                              StabilityController::PID_KP, 
                              StabilityController::PID_KI, 
                              StabilityController::PID_KD, 
                              DIRECT
                            );
-  this->_PIDPitchNeg = new PID(&this->_pid_pitchn_input, 
-                            &this->_pid_pitchn_output, 
-                            &this->_pid_pitchn_setpoint, 
+  this->_PIDPitchOpp = new PID(&this->_pid_pitchopp_input, 
+                            &this->_pid_pitchopp_output, 
+                            &this->_pid_pitchopp_setpoint, 
                             StabilityController::PID_KP, 
                             StabilityController::PID_KI, 
                             StabilityController::PID_KD, 
                             DIRECT
                           );
-  this->_PIDRollNeg = new PID(&this->_pid_rolln_input, 
-                             &this->_pid_rolln_output, 
-                             &this->_pid_rolln_setpoint, 
+  this->_PIDRollOpp = new PID(&this->_pid_rollopp_input, 
+                             &this->_pid_rollopp_output, 
+                             &this->_pid_rollopp_setpoint, 
                              StabilityController::PID_KP, 
                              StabilityController::PID_KI, 
                              StabilityController::PID_KD, 
                              DIRECT
                            );
 
-  this->_PIDPitchPos->SetMode(AUTOMATIC);
-  this->_PIDPitchNeg->SetMode(AUTOMATIC);
-  this->_PIDRollPos->SetMode(AUTOMATIC);
-  this->_PIDRollNeg->SetMode(AUTOMATIC);
+  this->_PIDPitch->SetMode(AUTOMATIC);
+  this->_PIDRoll->SetMode(AUTOMATIC);
+  this->_PIDPitchOpp->SetMode(AUTOMATIC);
+  this->_PIDRollOpp->SetMode(AUTOMATIC);
 }
 
 /**
- * output range -1 to 1 for each
+ * PRY output as a percentage, range -1 to 1 for each -jkr
+ * eg. -1 pitch means rear motors are high and vice-versa
+ * values are allowed to momentarily jump above 1 and below 0
  */
-double* StabilityController::getPRYOutput() {
+float* StabilityController::getOutput() {
    /**
    * get gyro output
    */
 
-  double gyro_x, gyro_y, gyro_z;
-  double* o = this->_imu->getGroundedGyroOutput();
+  float gyro_x, gyro_y, gyro_z;
+  float* o = this->_imu->getGyro();
   gyro_x = o[0];
   gyro_y = o[1];
   gyro_z = o[2];
@@ -108,59 +116,63 @@ double* StabilityController::getPRYOutput() {
   }
 
   /**
-   * get PRY output
+   * get PID accelerometer
    */
-   
-  double ppos = this->_pid_pitchp_output / PID_OUTPUT_MAX;
-  double rpos = this->_pid_rollp_output / PID_OUTPUT_MAX;
-  double pneg = this->_pid_pitchn_output / PID_OUTPUT_MAX;
-  double rneg = this->_pid_rolln_output / PID_OUTPUT_MAX;
-  double yaw = this->_r_yaw / YAW_MAX;
 
-  double gyro_x_comp = gyro_x * StabilityController::GYRO_MULT_PITCH;
-  double gyro_y_comp = gyro_y * StabilityController::GYRO_MULT_ROLL;
-  double gyro_z_comp = gyro_z * StabilityController::GYRO_MULT_YAW;
+  // change pid values to 0:1
+  float p = (this->_pid_pitch_output / PID_OUTPUT_MAX) * StabilityController::PID_MULT;
+  float r = (this->_pid_roll_output / PID_OUTPUT_MAX) * StabilityController::PID_MULT;
 
-  // add in the gyro values -jkr
+  // reduce by opposing PID's to make pids -1:1 -jkr
+  p -= (this->_pid_pitchopp_output / PID_OUTPUT_MAX) * StabilityController::PID_MULT;
+  r -= (this->_pid_rollopp_output / PID_OUTPUT_MAX) * StabilityController::PID_MULT;
+  
+  float y = this->_r_yaw / YAW_MAX;
 
-  double p = ppos - pneg - gyro_x_comp;
-  double r = rpos - rneg - gyro_y_comp;
-  double y = yaw - gyro_z_comp;
+  /**
+   * adjust pr amounts based on which one is needed more 
+   * high pitch is positive, high roll is negative -jkr
+   */
+  float pa = this->_pid_pitch_input / _pid_roll_input;
+  float ra = this->_pid_roll_input / _pid_pitch_input;
 
-//  override output to only gyro
-//  p = 0 - gyro_x_comp;
-//  r = 0 - gyro_y_comp;
-//  y = 0 - gyro_z_comp;
-
+  // factor in the results into the p and r values
+  if(abs(pa) > abs(ra)) r = r / abs(pa);
+  else p = p / abs(ra);
+  
   if(false) {
-    Serial.print(gyro_x_comp);
-    Serial.print(F("\t"));
-    Serial.print(gyro_y_comp);
-    Serial.print(F("\t"));
-    Serial.println(gyro_z_comp);
+    Serial.print(pa);
+    Serial.print(F(" "));
+    Serial.print(ra);
+    Serial.print(F("\t\t"));
+    Serial.print(r / abs(pa));
+    Serial.print(F(" "));
+    Serial.print(p / abs(ra));
+    Serial.print(F("\t\t"));
+    Serial.print(p);
+    Serial.print(F(" "));
+    Serial.println(r);
   }
 
-  // make sure values don't go outside of what is expected -jkr
-  // commenting this out to pave way for a max buffering system in EC -jkr
-  
-//  if(p > 1)   p = 1;
-//  if(p < -1)  p = -1;
-//
-//  if(r > 1)   r = 1;
-//  if(r < -1)  r = -1;
-//
-//  if(y > 1)   y = 1;
-//  if(y < -1)  y = -1;
+  float gyro_x_comp = gyro_x * StabilityController::GYRO_MULT_PITCH;
+  float gyro_y_comp = gyro_y * StabilityController::GYRO_MULT_ROLL;
+  float gyro_z_comp = gyro_z * StabilityController::GYRO_MULT_YAW;
+
+  // combine gyro values, attitude values, and pid values -jkr
+
+  p -= gyro_x_comp;
+  r -= gyro_y_comp;
+  y -= gyro_z_comp;
 
   if(false) {
     Serial.print(p);
     Serial.print(F("\t"));
     Serial.print(r);
     Serial.print(F("\t"));
-    Serial.println(y);
+    Serial.println(y); // if gyro is off, this value will always be 0 -jkr
   }
   
-  return new double[3] {p, r, y};
+  return new float[3] {p, r, y};
 }
 
 void StabilityController::loop() {
@@ -169,8 +181,8 @@ void StabilityController::loop() {
    * get desired drone position
    */
   
-  double* o;
-  double r_pitch = 0, r_roll = 0, r_yaw = 0;
+  float* o;
+  float r_pitch = 0, r_roll = 0, r_yaw = 0;
 
   if(this->_ros->isOn()) {
     o = this->_ros->getAttitude();
@@ -180,7 +192,7 @@ void StabilityController::loop() {
     delete [] o;
   }
   
-  // rc takes precedence
+  // rc takes precedence -jkr
   if(this->_rc->isOn()) {
     o = this->_rc->getAttitude();
     r_pitch = o[0];
@@ -203,14 +215,14 @@ void StabilityController::loop() {
    * get actual drone position
    */
   
-  o = this->_imu->getFilteredAttitude();
-  double imu_pitch, imu_roll, imu_yaw;
+  o = this->_imu->getAttitude();
+  float imu_pitch, imu_roll, imu_yaw;
   imu_pitch = o[0];
   imu_roll  = o[1];
   imu_yaw   = o[2];
   delete [] o;
 
-  if(true) {
+  if(false) {
     Serial.print(F("imu: "));
     Serial.print(imu_pitch);
     Serial.print(F(" "));
@@ -223,48 +235,43 @@ void StabilityController::loop() {
    * compare actual position vs desired position and calculate PID
    */
 
-  this->_pid_pitchp_input = imu_pitch;
-  this->_pid_pitchn_input = r_pitch;
-  this->_pid_rollp_input = imu_roll;
-  this->_pid_rolln_input = r_roll;
+  this->_pid_pitch_input = imu_pitch;
+  this->_pid_roll_input = imu_roll;
+  this->_pid_pitchopp_input = r_pitch;
+  this->_pid_rollopp_input = r_roll;
 
-  this->_pid_pitchp_setpoint = r_pitch;
-  this->_pid_pitchn_setpoint = imu_pitch;
-  this->_pid_rollp_setpoint = r_roll;
-  this->_pid_rolln_setpoint = imu_roll;
+  this->_pid_pitch_setpoint = r_pitch;
+  this->_pid_roll_setpoint = r_roll;
+  this->_pid_pitchopp_setpoint = imu_pitch;
+  this->_pid_rollopp_setpoint = imu_roll;
 
   this->_r_yaw = r_yaw;
 
   // might need to change pid settings when we are close to the goal -jkr
-  this->_PIDPitchPos->Compute();
-  this->_PIDRollPos->Compute();
-  this->_PIDPitchNeg->Compute();
-  this->_PIDRollNeg->Compute();
+  this->_PIDPitch->Compute();
+  this->_PIDRoll->Compute();
+  this->_PIDPitchOpp->Compute();
+  this->_PIDRollOpp->Compute();
 
   if(false) {// && this->_rc->isOn()) {
-    Serial.print(F("sc pid roll: "));
-    Serial.print(this->_pid_rollp_input);
+    Serial.print(this->_pid_pitch_input);
     Serial.print(F(" "));
-    Serial.print(this->_pid_rollp_setpoint);
+    Serial.print(this->_pid_pitch_setpoint);
     Serial.print(F(" "));
-    Serial.print(this->_pid_rollp_output);
+    Serial.print(this->_pid_pitch_output);
     Serial.print(F("\t\t"));
-    Serial.print(this->_pid_rolln_input);
+    Serial.print(this->_pid_pitchopp_input);
     Serial.print(F(" "));
-    Serial.print(this->_pid_rolln_setpoint);
+    Serial.print(this->_pid_pitchopp_setpoint);
     Serial.print(F(" "));
-    Serial.println(this->_pid_rolln_output);
+    Serial.println(this->_pid_pitchopp_output);
   }
 
-  // check the outputs
+  // check the outputs only
   if(false) {
-    Serial.print(this->_pid_pitchp_output);
+    Serial.print(this->_pid_pitch_output);
     Serial.print(F(" "));
-    Serial.print(this->_pid_rollp_output);
-    Serial.print(F("\t"));
-    Serial.print(this->_pid_pitchn_output);
-    Serial.print(F(" "));
-    Serial.println(this->_pid_rolln_output);
+    Serial.println(this->_pid_roll_output);
   }
 }
 
